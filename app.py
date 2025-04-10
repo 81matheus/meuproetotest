8c234458d75f7fe88e278ef7c31666904e5715ac
81matheus <matheusdelimacosta@gmail.com>
2025-04-10 13:58:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,15 @@ st.write("""
     Este aplicativo permite analisar estratégias de apostas com base em dados históricos.
     Estatística quantitativa é um método que usa números para testar hipóteses e estabelecer relações entre variáveis.
     Com base em evidências, permite fazer previsões e tirar conclusões. 
+
     
+    * Newton buscava leis universais que descrevessem o movimento e as interações. Ele focava em:
+    1. Forças e Equilíbrio: Identificar as "forças" em jogo (probabilidades implícitas) e procurar situações de desequilíbrio previsível ou de forte tendência para um estado específico.
+    2. Causa e Efeito: Relacionar condições iniciais (odds e suas relações) com resultados prováveis.
+    3. Leis Matemáticas: Expressar essas relações através de regras matemáticas claras e quantificáveis (nossas condições de filtro).
+    4. Inércia: Um sistema tende a permanecer em seu estado (por exemplo, um jogo tende a ser de poucos gols se as "forças" apontarem nessa direção).          
+    
+  
     Use a barra lateral para navegar entre as páginas:
     - **Dashboard**: Visão geral dos dados.
     
